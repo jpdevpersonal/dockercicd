6738052015981c26f1f5e9e1e1aa18471ac53531
Julian  Pickard <jpickard@and.digital>
2024-08-03 16:09:27 +0100

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Step 1: Build the React application
+FROM node:14 AS dist
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm install
+COPY . ./
+RUN npm run build
+
+# Step 2: Serve the application using Nginx
+FROM nginx:alpine
+COPY --from=dist /app/dist /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    console.log("hello world");
+  }, []);
+  function handleClick() {
+    console.log("hello");
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <h1>Deployed by CICD</h1>
+        <button onClick={handleClick}>Click me</button>
       </div>
     </>
   );
