28d99339be7814d5a89e0b7d79502db134caddf3
Julian  Pickard <jpickard@and.digital>
2024-09-15 16:13:57 +0100

    Manual deploy to azure

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <h1>Built and Deployed by Github Actions</h1>
-        <h1>Version 9</h1>
+        <h1>Version 10</h1>
         <button onClick={handleClick}>Click me</button>
       </div>
     </>
