d6bb48a731fe8b4596586be207f6f31d9dabb754
Julian  Pickard <jpickard@and.digital>
2024-09-15 19:54:15 +0100

    Removed checkouts

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Docker CICD Test</h1>
       <div className="card">
         <h1>Built and Deployed by Github Actions</h1>
-        <h1>Version 10</h1>
+        <h1>Version 11</h1>
         <button onClick={handleClick}>Click me</button>
       </div>
     </>
