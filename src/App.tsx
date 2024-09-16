4f728d01a566e84be684899a9247dcb3cd26bd87
Julian  Pickard <jpickard@and.digital>
2024-09-16 17:11:37 +0100

    Removed checkout in buildDockerImage

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <h1>Docker CICD Test</h1>
       <div className="card">
         <h1>Built and Deployed by Github Actions</h1>
-        <h1>Version 11</h1>
+        <h1>Version 12</h1>
         <button onClick={handleClick}>Click me</button>
       </div>
     </>
