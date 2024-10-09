db3809e325f44b88efdbc81ac47b1317a7bf8c31
Julian  Pickard <jpickard@and.digital>
2024-10-09 09:29:03 +0100

    Fixed Tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,19 +6,17 @@ describe("App Component", () => {
   test("renders the component correctly", () => {
     render(<App />);
     expect(screen.getByText("Docker CICD Test")).toBeInTheDocument();
+    expect(screen.getByText("Manual Docker Build")).toBeInTheDocument();
     expect(
-      screen.getByText("Built and Deployed by Github Actions")
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: /click me/i })
+      screen.getByRole("button", { name: /Log To Console/i })
     ).toBeInTheDocument();
   });
 
   test("button click triggers handleClick function", () => {
     console.log = jest.fn();
     render(<App />);
-    const button = screen.getByRole("button", { name: /click me/i });
+    const button = screen.getByRole("button", { name: /Log To Console/i });
     fireEvent.click(button);
-    expect(console.log).toHaveBeenCalledWith("hello");
+    expect(console.log).toHaveBeenCalledWith("Hello");
   });
 });
