97e6c9ee895c9f6fe9e2296906e79d4df1bf90bc
Julian  Pickard <jpickard@and.digital>
2024-10-15 21:58:29 +0100

    Ready to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useEffect } from "react";
 
 function App() {
-  const app_version = import.meta.env.VITE_APP_VERSION;
-
   useEffect(() => {
     console.log("hello world");
   }, []);
   function handleClick() {
-    console.log("Version" + { app_version });
+    console.log("Hello");
   }
 
   return (
@@ -16,7 +14,7 @@ function App() {
       <div className="card">
         <h1>Manual checkout</h1>
         <h1>Manual Docker Build</h1>
-        <h2>Version: {app_version}</h2>
+        <h2>Version: 10</h2>
         <button onClick={handleClick}>Log To Console</button>
       </div>
     </>
