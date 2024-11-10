d3d601d53ebfc23d53c7801a6df96fef117121fe
Julian  Pickard <jpickard@and.digital>
2024-11-10 16:23:50 +0000

    deploy main to prod

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
         <h1>Deploy To Development</h1>
         <h1>Merge into main</h1>
         <h1>Deploy To Production</h1>
-        <h2>Version: 1</h2>
+        <h2>Version: 2</h2>
         <h1>Development</h1>
         <button onClick={handleClick}>Log To Console</button>
       </div>
