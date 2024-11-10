f4e3443fbfe629916ffd97467fe37f88db923392
Julian  Pickard <jpickard@and.digital>
2024-11-10 15:55:24 +0000

    deploy main to prod

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,7 +6,6 @@ describe("App Component", () => {
   test("renders the component correctly", () => {
     render(<App />);
     expect(screen.getByText("Docker CICD Test")).toBeInTheDocument();
-    expect(screen.getByText("Manual Docker Build")).toBeInTheDocument();
     expect(
       screen.getByRole("button", { name: /Log To Console/i })
     ).toBeInTheDocument();
