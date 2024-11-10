76839f8f39ef26596bc6bde0554a0e773f379036
Julian  Pickard <jpickard@and.digital>
2024-11-10 15:51:25 +0000

    deploy to development and merge with main

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
     <>
       <h1>Docker CICD Test</h1>
       <div className="card">
-        <h1>Manual checkout</h1>
-        <h1>Manual Docker Build</h1>
+        <h1>Deploy To Development</h1>
+        <h1>Merge into main</h1>
         <h2>Version: 1</h2>
         <h1>Development</h1>
         <button onClick={handleClick}>Log To Console</button>
