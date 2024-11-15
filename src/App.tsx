5f7964ce1dcee6be8e22fc27c93b66fb81f6da0b
Julian  Pickard <jpickard@and.digital>
2024-11-15 11:17:31 +0000

    Live

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <h1>Docker CICD Test</h1>
       <div className="card">
         <h1>Deploy To Production</h1>
-        <h2>Version: 3</h2>
+        <h2>Live</h2>
         <h1>Development</h1>
         <button onClick={handleClick}>Log To Console</button>
       </div>
